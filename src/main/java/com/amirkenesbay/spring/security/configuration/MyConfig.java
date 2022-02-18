39f2cb8f6d16fa25eb788e479bf3c12d4e846072
amirkenesbay <amirkenesbay@gmail.com>
2022-02-18 19:02:01 +0600

    Add bcrypt encoder for passwords

--- src/main/java/com/amirkenesbay/spring/security/configuration/MyConfig.java
+++ src/main/java/com/amirkenesbay/spring/security/configuration/MyConfig.java
@@ -1,5 +1,7 @@
 package com.amirkenesbay.spring.security.configuration;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -7,16 +9,33 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+
 @Configuration
 @ComponentScan(basePackages = "com.amirkenesbay.spring.security")
 @EnableWebMvc
 public class MyConfig {
-
     @Bean
     public ViewResolver viewResolver(){
         InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
         internalResourceViewResolver.setPrefix("/WEB-INF/view/");
         internalResourceViewResolver.setSuffix(".jsp");
         return internalResourceViewResolver;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false");
+            dataSource.setUser("bestuser");
+            dataSource.setPassword("bestuser");
+
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+        return dataSource;
     }
 }
--- src/main/java/com/amirkenesbay/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/amirkenesbay/spring/security/configuration/MySecurityConfig.java
@@ -1,28 +1,45 @@
 package com.amirkenesbay.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder
-                        .username("amir")
-                        .password("zaur")
-                        .roles("EMPLOYEE"))
-                .withUser(userBuilder
-                        .username("alizhan")
-                        .password("alizhan")
-                        .roles("HR"))
-                .withUser(userBuilder
-                        .username("alima")
-                        .password("alima")
-                        .roles("MANAGER", "HR"));
+        auth.jdbcAuthentication().dataSource(dataSource);
+//        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//        auth.inMemoryAuthentication()
+//                .withUser(userBuilder
+//                        .username("amir")
+//                        .password("zaur")
+//                        .roles("EMPLOYEE"))
+//                .withUser(userBuilder
+//                        .username("alizhan")
+//                        .password("alizhan")
+//                        .roles("HR"))
+//                .withUser(userBuilder
+//                        .username("alima")
+//                        .password("alima")
+//                        .roles("MANAGER", "HR"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").hasAnyRole("EMPLOYEE", "HR", "MANAGER")
+                .antMatchers("/hr_info").hasRole("HR")
+                .antMatchers("/manager_info/**").hasRole("MANAGER")
+                .and().formLogin().permitAll();
     }
 }
